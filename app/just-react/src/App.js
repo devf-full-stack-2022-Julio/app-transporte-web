e5a5cd812983fb5497aa87d34aa3d555e947996b
Misael Calvillo Mancilla <misa@homely.mx>
2022-07-13 22:08:02 -0500

    change app content

--- app/just-react/src/App.js
+++ app/just-react/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola mundo desde app de react ✅
         </p>
         <a
           className="App-link"
