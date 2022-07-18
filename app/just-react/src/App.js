c4c105e64129451edffad31a42ab6acbb8c28a41
Miguel Ángel García Martínez <ma.garmar98@gmail.com>
2022-07-17 21:55:45 -0500

    feat: first draft for sign up

--- app/just-react/src/App.js
+++ app/just-react/src/App.js
@@ -8,17 +8,26 @@ function App() {
   const [password, setPassword] = useState('')
   const [error, setError] = useState(null)
 
-  console.log(user)
 
-  function onEmailChangeHandler(e) {
+  
+
+  /* function onEmailChangeHandler(e) {
+    setEmail(e.target.value)
+  } */
+
+ /*  function onPasswordChangeHandler(e) {
+    setPassword(e.target.value)
+  } */
+
+  function onNewEmailChangeHandler(e) {
     setEmail(e.target.value)
   }
 
-  function onPasswordChangeHandler(e) {
+  function onNewPasswordChangeHandler(e) {
     setPassword(e.target.value)
   }
 
-  function onLoginButtonClickHandler() {
+/*   function onLoginButtonClickHandler() {
     if (!email) {
       setError('Ingresa tu email')
       return
@@ -53,20 +62,59 @@ function App() {
           setError(null)
         }) 
       })
+  } */
+
+  function onSignUpButtonClickHandler() {
+    if (!email) {
+      setError('Ingresa tu email')
+      return
+    }
+    if (!password) {
+      setError('Ingresa tu password')
+      return 
+    } 
+
+    fetch('http://localhost:8000/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email,
+          password
+        })
+      })
+      .then((res) => {
+        if (res.status >= 400) {
+          console.error('Respuesta con código de error 🚫')
+          res.json().then((error) => {
+            setError(error.message)
+            setUser(null)
+          })
+          return;
+        }
+        console.log('Respuesta recibida! ✅')
+        res.json().then((newUser) => {
+          setUser(newUser)
+          setError(null)
+        }) 
+      })
   }
 
-  function onEnterHandler(key) {
+
+  /* function onEnterHandler(key) {
     if (key.code === "Enter") {
-      onLoginButtonClickHandler()
+       onLoginButtonClickHandler() 
     }
-  }
+  } */
+
 
   // console.count('Se renderiza componente App')
   return (
     <div>
       <h1 id="title" className="title">Bienvenidos a mi app 🚀</h1>
 
-      <form>
+      {/* <form>
         <input 
           type="email" 
           placeholder="Ingresa tu email" 
@@ -87,7 +135,26 @@ function App() {
 
       <div>
         {user && <h3>Usuario: {user.token}</h3>}
-      </div>
+      </div> */}
+
+      <form>
+        <input 
+          type="email" 
+          placeholder="Ingresa tu email" 
+          value={email}
+          onChange={onNewEmailChangeHandler}
+          /* onKeyDown={onEnterHandler} */
+        />
+        <input 
+          type="password" 
+          placeholder="Ingresa tu password" 
+          value={password}
+          onChange={onNewPasswordChangeHandler}
+          /* onKeyDown={onEnterHandler} */
+        />
+        <button type="button" onClick={onSignUpButtonClickHandler}>Ingresar</button>
+        <p className="error-message">{error}</p>
+      </form>      
     </div>
   );
 }
